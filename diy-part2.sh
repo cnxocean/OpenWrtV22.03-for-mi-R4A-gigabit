815fc1581c263d8136b23db91e8a6d8bea395f4a
cnxocean <xocean@126.com>
2022-11-16 16:05:46 +0800

    update change defoult theme

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+# Change default theme
+sed -i 's/default Bootstrap theme/default Argon theme/g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci/Makefile
